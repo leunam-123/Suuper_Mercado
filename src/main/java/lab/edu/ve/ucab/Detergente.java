257b73b7b5701ad9efaffbd3595e07413df35939
Manuel Antias <meantias.23@est.ucab.edu.ve>
2024-10-13 14:03:36 -0400

    Agregue los métodos para aplicar y pedir el descuento

--- src/main/java/lab/edu/ve/ucab/Detergente.java
+++ src/main/java/lab/edu/ve/ucab/Detergente.java
@@ -2,85 +2,71 @@ package lab.edu.ve.ucab;
 
 import java.util.Scanner;
 
-public class Detergente implements EsLiquido {
+public class Detergente implements EsLiquido,ConDescuento {
     private String marca;
     private double precio;
     private double volumen;
     private String tipoEnvase;
-    private double descuento;
+    private double precioDescuento;
 
     Scanner datos = new Scanner(System.in);
     Scanner cadenas = new Scanner(System.in);
 
-    public Detergente(String marca, double precio) {
-        volumen = 0.0;
+    public Detergente(String marca, double precio) { //Constructor que inicializa los valores de los atributos
+        volumen = 0.0;                               //Por parámetros
         tipoEnvase = "";
         this.marca = marca;
         this.precio = precio;
+        precioDescuento= 0.0;
     }
 
     @Override
-    public double getVolumen() {
+    public double getVolumen() { //Pido el volúmen del producto solicitado
         System.out.println("Por favor ingrese el volumen del producto: ");
         volumen = cadenas.nextDouble();
         return volumen;
     }
 
     @Override
-    public void setVolumen(double volumen) {
+    public void setVolumen(double volumen) { //imprimo el valor del Volumen ingresado
         System.out.println("El volumen de su producto es : "+volumen+" metros cubicos");
     }
 
 
     @Override
-    public String getTipoEnvase() {
+    public String getTipoEnvase() { //Le pido al usuario el tipo de envase del producto y lo retorno
         System.out.println("Por favor ingrese el tipo de envase (Grande, pequenio o mediano): ");
         tipoEnvase=cadenas.next();
         return tipoEnvase;
     }
 
     @Override
-    public void setTipoEnvase(String tipoEnvase) {
+    public void setTipoEnvase(String tipoEnvase) { //Imprimo el tipo de Envase del producto
         System.out.println("Su producto posee un envase: " + tipoEnvase);
     }
 
-    public void aplicarDescuento() {
-        double descuento;
-        double precioDescuento;
+
+    @Override
+    public double getDescuento() { //Le pido al usuario el porcentaje de descuento de su producto
+        double descuento=0.0;
         System.out.print("Por favor ingrese cual es el su porcentaje de descuento?: %");
         descuento=datos.nextDouble();
         System.out.println(" ");
         System.out.println("Ahora mismo se aplicara el descuento al precio de su producto");
-        descuento=descuento/100;
-        precioDescuento=precio*descuento;
-        precio=precio-precioDescuento;
-        System.out.println(" ");
+        return descuento;
     }
 
-    public void pedirDescuento(){
-        String opcion;
-        do{
-            System.out.println("¿Posee usted un descuento? Seleccione las alternativas propuestas: ");
-            System.out.println(" ");
-            System.out.println("1. Poseo descuento");
-            System.out.println(" ");
-            System.out.println("2. No poseo descuento");
-            opcion=cadenas.next();
-            if (!"1".equals(opcion) && !"2".equals(opcion)){
-                System.out.println(" ");
-                System.out.println("ERROR");
-                System.out.println("La opcion seleccionada fue: "+opcion);
-                System.out.println("Y dicha opción no está dentro de las propuestas anteriormente");
-                System.out.println("Intentelo de nuevo.");
-            }
-        }while (!"1".equals(opcion) && !"2".equals(opcion));
-        if ("1".equals(opcion)){
-            aplicarDescuento();
-        }
-        else if ("2".equals(opcion)){
-            System.out.println("Su precio se mantiene igual");
-            System.out.println(" ");
-        }
+    @Override
+    public void setDescuento(double descuento) { //Aplico el descuento al precio, y lo guardo en un atributo
+        descuento=descuento/100;
+        precioDescuento=precio*descuento;
+        precioDescuento=precio-precioDescuento;
+    }
+
+    @Override
+    public double getPrecioDescuento() { //Inicializo los métodos de pedir el descuento y aplicarlo
+        setDescuento(getDescuento());    //Uno por parámetro para aplicar los datos correspondientes
+        return precio=precioDescuento;  //Anexo el PrecioDescuento a precio
     }
 
     public void tooString() {
@@ -111,4 +97,6 @@ public class Detergente implements EsLiquido {
     public void setMarca(String marca) {
         this.marca = marca;
     }
+
+
 }
--- src/main/java/lab/edu/ve/ucab/Main.java
+++ src/main/java/lab/edu/ve/ucab/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner datos=new Scanner(System.in);
         Scanner cadenas=new Scanner(System.in);
+        String opcion=" ";
 
         System.out.println("Por favor ingrese la marca del Detergente que desea consultar: ");
         String marca=datos.next();
@@ -17,7 +18,29 @@ public class Main {
         Detergente Deter=new Detergente(marca,precio);
         Deter.getVolumen();
         Deter.getTipoEnvase();
-        Deter.pedirDescuento();
+
+        do{
+            System.out.println("¿Posee usted un descuento? Seleccione las alternativas propuestas: ");
+            System.out.println(" ");
+            System.out.println("1. Poseo descuento");
+            System.out.println(" ");
+            System.out.println("2. No poseo descuento");
+            opcion=cadenas.next();
+            if (!"1".equals(opcion) && !"2".equals(opcion)){
+                System.out.println(" ");
+                System.out.println("ERROR");
+                System.out.println("La opcion seleccionada fue: "+opcion);
+                System.out.println("Y dicha opción no está dentro de las propuestas anteriormente");
+                System.out.println("Intentelo de nuevo.");
+            }
+        }while (!"1".equals(opcion) && !"2".equals(opcion));
+        if ("1".equals(opcion)){
+            Deter.getPrecioDescuento();
+        }
+        else if ("2".equals(opcion)){
+            System.out.println("Su precio se mantiene igual");
+            System.out.println(" ");
+        }
         Deter.tooString();
     }
 }
