0109aba6087921fa38556c91af1f438a64322914
Manuel Antias <meantias.23@est.ucab.edu.ve>
2024-10-21 20:35:02 -0400

    Eliminación de complejidad innecesaria

--- src/main/java/lab/edu/ve/ucab/Main.java
+++ src/main/java/lab/edu/ve/ucab/Main.java
@@ -39,83 +39,79 @@ public class Main {
                 System.out.println("Intentelo de nuevo.");
             }
         }while (opcion!='1' && opcion!='2' && opcion!='3' && opcion!='4');
-            if(opcion=='1'||opcion=='2'||opcion=='3'||opcion=='4'){
-                switch(opcion){
-                    case '1':
-                        System.out.println("\033[35mPor favor digite los siguientes datos del producto: ");
-                        System.out.println(" ");
-                        System.out.println("Ingrese la marca del Producto: ");
-                        marca=cadenas.next();
-                        System.out.println("Por favor ingrese el precio del Detergente que desea consultar (decimales con ,): ");
-                        precio=datos.nextDouble();
-                        Detergente Deter=new Detergente(marca,precio);
-                        Deter.getVolumen();
-                        Deter.getTipoEnvase();
-                        Deter.pedirOpcionesDescuento();
-                        Deter.tooString();
-                        break;
+        switch (opcion) {
+            case '1':
+                System.out.println("\033[35mPor favor digite los siguientes datos del producto: ");
+                System.out.println(" ");
+                System.out.println("Ingrese la marca del Producto: ");
+                marca = cadenas.next();
+                System.out.println("Por favor ingrese el precio del Detergente que desea consultar (decimales con ,): ");
+                precio = datos.nextDouble();
+                Detergente Deter = new Detergente(marca, precio);
+                Deter.getVolumen();
+                Deter.getTipoEnvase();
+                Deter.pedirOpcionesDescuento();
+                Deter.tooString();
+                break;
 
-                    case '2':
+            case '2':
 
-                        System.out.println("Por favor digite los siguientes datos del producto: ");
-                        System.out.println(" ");
-                        System.out.println("Ingrese la marca del Producto: ");
-                        marca = cadenas.next();
-                        System.out.println("Por favor ingrese el precio del Producto (Decimales con ,): ");
-                        precio=datos.nextDouble();
-                        System.out.println("Por favor ingrese alguno de los tipos de cereales presentados: ");
-                        System.out.println(" ");
-                        System.out.println("* Cereal Espelta");
-                        System.out.println(" ");
-                        System.out.println("* Cereal de Maiz");
-                        System.out.println(" ");
-                        System.out.println("* Cereal de Trigo");
-                        System.out.println(" ");
-                        System.out.println("* Otro (Especificar)");
-                        String cereal=cadenas.next().toLowerCase();
-                        if (cereal.equals("espelta")){
-                            Cereal cere=new Cereal(marca,precio,cereal);
-                            cere.tooString();
-                        } else if (cereal.equals("maiz")) {
-                            Cereal cere=new Cereal(marca,precio,cereal);
-                            cere.tooString();
-                        }
-                        else if (cereal.equals("trigo")) {
-                            Cereal cere=new Cereal(marca,precio,cereal);
-                            cere.tooString();
-                        }
-                        else {
-                            Cereal cere=new Cereal(marca,precio,cereal);
-                            cere.tooString();
-                        }
-
-                        break;
-
-                    case '3':
-                        System.out.println("Por favor digite los siguientes datos del producto: ");
-                        System.out.println(" ");
-                        System.out.println("Ingrese la marca del Producto: ");
-                        marca = cadenas.next();
-                        System.out.println("Por favor ingrese el precio del Producto (Decimales con ,): ");
-                        precio=datos.nextDouble();
-                        System.out.println("Por favor ingrese el tipo de Vino: ");
-                        String tipoDeVino=cadenas.next();
-                        System.out.println(" ");
-                        System.out.println("Ingrese el grado del Alcohol del producto: ");
-                        double grado=datos.nextDouble();
-                        Vino vin= new Vino(marca,tipoDeVino,grado,precio);
-                        vin.getVolumen();
-                        vin.getTipoEnvase();
-                        vin.pedirOpcionesDescuento();
-                        vin.tooString();
-                        break;
-
-                    case '4':
-                        System.out.println("\033[36mGracias por usar el programa! tenga buen dia!");
-                        break;
-
+                System.out.println("Por favor digite los siguientes datos del producto: ");
+                System.out.println(" ");
+                System.out.println("Ingrese la marca del Producto: ");
+                marca = cadenas.next();
+                System.out.println("Por favor ingrese el precio del Producto (Decimales con ,): ");
+                precio = datos.nextDouble();
+                System.out.println("Por favor ingrese alguno de los tipos de cereales presentados: ");
+                System.out.println(" ");
+                System.out.println("* Cereal Espelta");
+                System.out.println(" ");
+                System.out.println("* Cereal de Maiz");
+                System.out.println(" ");
+                System.out.println("* Cereal de Trigo");
+                System.out.println(" ");
+                System.out.println("* Otro (Especificar)");
+                String cereal = cadenas.next().toLowerCase();
+                if (cereal.equals("espelta")) {
+                    Cereal cere = new Cereal(marca, precio, cereal);
+                    cere.tooString();
+                } else if (cereal.equals("maiz")) {
+                    Cereal cere = new Cereal(marca, precio, cereal);
+                    cere.tooString();
+                } else if (cereal.equals("trigo")) {
+                    Cereal cere = new Cereal(marca, precio, cereal);
+                    cere.tooString();
+                } else {
+                    Cereal cere = new Cereal(marca, precio, cereal);
+                    cere.tooString();
                 }
-            }
+
+                break;
+
+            case '3':
+                System.out.println("Por favor digite los siguientes datos del producto: ");
+                System.out.println(" ");
+                System.out.println("Ingrese la marca del Producto: ");
+                marca = cadenas.next();
+                System.out.println("Por favor ingrese el precio del Producto (Decimales con ,): ");
+                precio = datos.nextDouble();
+                System.out.println("Por favor ingrese el tipo de Vino: ");
+                String tipoDeVino = cadenas.next();
+                System.out.println(" ");
+                System.out.println("Ingrese el grado del Alcohol del producto: ");
+                double grado = datos.nextDouble();
+                Vino vin = new Vino(marca, tipoDeVino, grado, precio);
+                vin.getVolumen();
+                vin.getTipoEnvase();
+                vin.pedirOpcionesDescuento();
+                vin.tooString();
+                break;
+
+            case '4':
+                System.out.println("\033[36mGracias por usar el programa! tenga buen dia!");
+                break;
+
+        }
 
 
     }
